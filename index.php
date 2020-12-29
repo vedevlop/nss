296d47a983caf84a591ff34b3634a29f3f79d6d5
vedevlop <75240914+vedevlop@users.noreply.github.com>
2020-12-29 14:17:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<!-- saved from url=(0049)file:///H:/NSS%20PROJECT/Projectfiles/index1.html -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NSS : National Service Scheme</title>
+<link rel="stylesheet" href="file:///H:/NSS%20PROJECT/Projectfiles/CSS/w3.css"><link rel="stylesheet" href="file:///H:/NSS%20PROJECT/Projectfiles/CSS/style.css"></head>
+<!-- linked css files -->
+
+
+
+<body class="w3-mobile back">
+
+
+    <!-- Page Heading-->
+    <div align="center" style="background-image: linear-gradient(120deg,rgb(235, 116, 5),rgb(82, 248, 82))">
+    <div style="font-family: Roboto, sans-serif ; width: 40% ; align:center;" class="w3-mobile w3-input w3-shadow w3-blue w3-badge  w3-white  w3-animate-zoom"><h3> Welocome to the  <b>NSS</b> : <i>National Service Scheme.... </i></h3>
+    </div>
+    <img src="file:///H:/NSS%20PROJECT/Projectfiles/images/NSSlogo.jpg" alt="NSSlogo" width="8%" height="8%" align="right" style="margin-top: -8%; border-radius: 55%" class="w3-spin">
+    
+</div>
+ 
+   <!-- Top bar Menu Section-->
+    <section class="w3-cell-row w3-cell-top w3-black w3-mobile">
+        <a href="file:///H:/NSS%20PROJECT/Projectfiles/index1.html"><input type="button" value="Home" class="w3-mobile w3-btn w3-animate-zoom w3-hover-green"></a>
+        <a href="file:///H:/NSS%20PROJECT/Projectfiles/images%20of%20nss.html" class="w3-mobile w3-btn w3-animate-zoom w3-hover-green">Images</a>
+        <a href="file:///H:/NSS%20PROJECT/Projectfiles/login.html" class="w3-mobile w3-btn w3-animate-zoom w3-hover-green">Login</a>
+        <a href="file:///H:/NSS%20PROJECT/Projectfiles/register.html" class="w3-mobile w3-btn w3-animate-zoom w3-hover-green">Register</a>
+
+
+       
+        <input type="search" name="search" id="search" placeholder="search here" style="border-radius: 25% ;margin-top: 0.3%;" class="w3-right w3-center w3-white">
+    </section>
+
+<!-- Page contents -->
+
+    <!-- NSS Slogan -->
+    <section class="w3-mobile w3-bar-item w3-cell-top w3-cell-row " style="background-image: linear-gradient(10deg, rgb(235, 193, 174), rgb(171, 240, 171) , rgb(203, 203, 255));">
+         <h2 class="w3-animated-zoom w3-center" style="font-family: Georgia, &#39;Times New Roman&#39;, Times, serif ;text-decoration-style: wavy;"> <marquee><i><b> NOT ME, BUT YOU</b></i></marquee></h2>
+    
+        </section>
+
+    <div class="back w3-mobile">
+        <div class="w3-bar-block">
+
+
+            <!-- Images OF NSS Section -->
+        <h3 class="w3-animate-zoom w3-black w3-center " id="imagesnss" style="font: 16px/20px Georgia, &#39;Times New Roman&#39;, Times, serif"><u> NSS &nbsp; IMAGES</u></h3>
+        <style>
+        .images{
+            height: 20%;
+            width: 80%;
+            box-shadow: 1px 2px 10px rgb(11, 26, 235);
+        }
+        </style>
+        <p style="margin-left: 2%">
+            <span style="width: 95%;height:20%;" class="w3-cell-row images">
+               </span></p><center>
+                <img src="file:///H:/NSS%20PROJECT/Projectfiles/images/nss1.jpg" style="border-radius: 15%" width="30%" height="15%">&nbsp;
+                <img src="file:///H:/NSS%20PROJECT/Projectfiles/images/NSS.png" style="border-radius: 15%">&nbsp;
+                <img src="file:///H:/NSS%20PROJECT/Projectfiles/images/nss2.jpg" style="border-radius: 15%" width="25%" height="10%">
+               </center>
+                 
+     
+        <p></p>
+
+       <p></p>
+
+
+        <!-- Abstract -->
+        <h3 class="w3-animate-zoom w3-black w3-center " id="Abstractnss" style="font: 16px/20px Georgia, &#39;Times New Roman&#39;, Times, serif"><u>Abstract</u></h3>
+       
+        <p> The National Service Scheme (NSS) is an Indian government-sponsored public service program conducted by the Ministry of Youth Affairs and Sports of the Government of India. Popularly known as NSS, the scheme was launched in Gandhiji's Centenary year in 1969. Aimed at developing student's personality through community service, NSS is a voluntary association of young people in Colleges, Universities and at +2 level working for a campus-community (esp. Villages) linkage.<br>
+           NSS volunteers work in rural areas, adopted villages and school to the serving the cause of society through survey, education and health awareness programme. This study involves in identifying the importance, aims and objectives of NSS in higher education which in turn create social responsibility among the students.</p>
+ 
+ 
+ 
+        <!-- History of NSS -->
+        <h3 class="w3-animate-zoom w3-black w3-center " id="historynss" style="font: 16px/20px Georgia, &#39;Times New Roman&#39;, Times, serif"><u>HISTORY OF NSS</u></h3>
+        <p style="margin-left: 16%">
+           </p><p>National Service Scheme (NSS) was launched during 1969, the birth centenary year of Mahatma Gandhi, in 37 universities involving 40000 students. NSS is an extension dimension to the higher education system to orient the student youth to community service while they are studying in educational institutions. It is being implemented by the Ministry of Youth Affairs and Sports, Government of India.
+ 
+         </p>
+         
+
+       <!-- Activity Callender -->
+       <h3 class="w3-animate-zoom w3-black w3-center " id="callendernss" style="font: 16px/20px Georgia, &#39;Times New Roman&#39;, Times, serif"><u> ANNUAL ACTIVITIES CALLENDER <br>AUSPICIOUS DAYS FOR ALL NSS UNITS</u></h3>
+       <p style="margin-left: 16%">
+        </p><table border="1px" align="center">
+            <tbody><tr><th width="5%">NO.</th>
+            <th width="40%">OCCASION</th>
+            <th width="30%">DATE &amp; MONTH</th>
+            </tr><tr><td>1</td>
+            <td>National Youth Day</td>
+            <td>12th January</td></tr>
+            <tr><td>2</td>
+            <td>National Youth Week</td>
+            <td>12th to 19 January</td></tr>
+            <tr><td>3</td>
+            <td>Republic Day</td>
+            <td>26th January</td></tr>
+            <tr><td>4</td>
+            <td>Martyr's Day</td>
+            <td>30th January</td></tr>
+            <tr><td>5</td>
+            <td>World Understanding Day</td>
+            <td>23rd February</td></tr>
+            <tr><td>6</td>
+            <td>Women's Day</td>
+            <td>8th March</td></tr>
+            <tr><td>7</td>
+            <td>World Forest Day</td>
+            <td>31st March</td></tr>
+            <tr><td>8</td>
+            <td>World Health Day</td>
+            <td>7th April</td></tr>
+            <tr><td>9</td>
+            <td>Fire Prevention Day</td>
+            <td>14th April</td></tr>
+            <tr><td>10</td>
+            <td>World Labour Day</td>
+            <td>1st May</td></tr>
+            <tr><td>11</td>
+            <td>Maharashtra Day</td>
+            <td>1st May</td></tr>
+            <tr><td>12</td>
+            <td>Nutrition Week</td>
+            <td>1st to 7th July</td></tr>
+            <tr><td>13</td>
+            <td>World Environment Day</td>
+            <td>5th June</td></tr>
+            <tr><td>14</td>
+            <td>Banamahotsav Week</td>
+            <td>1st to 7th July</td></tr>
+            <tr><td>15</td>
+            <td>World Population Day</td>
+            <td>11th July</td></tr>
+            <tr><td>16</td>
+            <td>University Foundation Day</td>
+            <td>18th July</td></tr>
+            <tr><td>17</td>
+            <td>Campaign Against Nuclear Weapon Day</td>
+            <td>6th August</td></tr>
+            <tr><td>18</td>
+            <td>Independence Day</td>
+            <td>15th August</td></tr>
+            <tr><td>19</td>
+            <td>Teacher's Day</td>
+            <td>5th September</td></tr>
+            <tr><td>20</td>
+            <td>International Literacy Day-Week</td>
+            <td>8th to 14th September</td></tr>
+            <tr><td>21</td>
+            <td>NSS Foundation Day</td>
+            <td>24th September</td></tr>
+            <tr><td>22</td>
+            <td>Gandhi Jayanti-Gandhi Week</td>
+            <td>2nd October</td></tr>
+            <tr><td>23</td>
+            <td>White Cane Day</td>
+            <td>15th October</td></tr>
+            <tr><td>24</td>
+            <td>World Food Day</td>
+            <td>16th October</td></tr>
+            <tr><td>25</td>
+            <td>U.N.O. Day</td>
+            <td>24th October</td></tr>
+            <tr><td>26</td>
+            <td>Traffic Week</td>
+            <td>24th to 30th October</td></tr>
+            <tr><td>27</td>
+            <td>Savings Day</td>
+            <td>31st October</td></tr>
+            <tr><td>28</td>
+            <td>National Integration Day</td>
+            <td>31st October</td></tr>
+            <tr><td>29</td>
+            <td>Children's Day</td>
+            <td>14th November</td></tr>
+            <tr><td>30</td>
+            <td>Mother's Day</td>
+            <td>19th November</td></tr>
+            <tr><td>31</td>
+            <td>Quami Ekta Week-Communal Harmony Campaign</td>
+            <td>19th to 25th November</td></tr>
+            <tr><td>32</td>
+            <td>Weeker's Selection Day</td>
+            <td>22nd November</td></tr>
+            <tr><td>33</td>
+            <td>Environment Awareness Month</td>
+            <td>19th Nov to 18th December </td></tr>
+            <tr><td>34</td>
+            <td>World AIDS Day</td>
+            <td>1st December</td></tr>
+            <tr><td>35</td>
+            <td>International Volunteers Day</td>
+            <td>5th December</td></tr>
+            <tr><td>36</td>
+            <td>Human Rights Day</td>
+            <td>10th December</td></tr>
+            <tr><td>37</td>
+            <td>Annual Special Camps</td>
+            <td>October-January</td></tr>
+    
+        </tbody></table>
+       <p></p>    
+        </div>
+    </div>
+
+</body></html>
